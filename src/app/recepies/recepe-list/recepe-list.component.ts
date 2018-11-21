002f2f73683f1d1e22032434a56f2a4119c7cb12
Aline Bossi <Aline@MacBook-Pro-de-Aline.local>
2018-11-21 21:27:33 -0200

    Ádding list
    Q!
    !
    >q!
    :q!
    :q!
    exit
    /

--- src/app/recepies/recepe-list/recepe-list.component.ts
+++ src/app/recepies/recepe-list/recepe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recepie } from '../recepie.model';
 
 @Component({
   selector: 'app-recepe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recepe-list.component.css']
 })
 export class RecepeListComponent implements OnInit {
+
+  recipes = [
+    // tslint:disable-next-line:max-line-length
+    new Recepie('teste', 'test', 'https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg')
+  ];
 
   constructor() { }
 
