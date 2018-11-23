c03009fb9ff4d87c0559183df7ba444f1fc8f757
Aline Bossi <Aline@MacBook-Pro-de-Aline.local>
2018-11-22 22:19:21 -0200

    Configuring the recipe and shopping component

--- src/app/recepies/recepe-list/recepe-list.component.ts
+++ src/app/recepies/recepe-list/recepe-list.component.ts
@@ -8,8 +8,9 @@ import { Recepie } from '../recepie.model';
 })
 export class RecepeListComponent implements OnInit {
 
-  recipes = [
+  recipes: Recepie[]  = [
     // tslint:disable-next-line:max-line-length
+    new Recepie('teste', 'test', 'https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg'),
     new Recepie('teste', 'test', 'https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg')
   ];
 
